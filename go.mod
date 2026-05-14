8e48f3b21fad435a5d25e1194357ef388cd2141c
KiranSatyaRaj <kiranjamy20021508@gmail.com>
2026-05-14 18:34:52 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KiranSatyaRaj/container-from-scratch
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// docker run image <cmd> <params>
+// go run main.go run    <cmd> <params>
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("bad command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func child() {
+	fmt.Printf("Running %v as %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
+	syscall.Chroot("./rootfs/")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
+	syscall.Unmount("/proc/", 0)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
